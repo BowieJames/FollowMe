75d1013490d7ea66ef2323c0ba0c780f001cdcf4
Nmc162 <ncox135@gmail.com>
2025-08-08 12:41:46 +1000

    add a way to change distance to speed
    also added values to functions to prepare for uwb code insertion

---
+++ motorcontrol/distos_added.ino
@@ -0,0 +1,97 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
+int currentAngle = 90; // middle position
+int currentspeed = 0;
+int targetspeed;
+int direction;
+int distance;
+// Motor driver pins
+const int ENA = 9;
+const int ENB = 10;
+const int IN1 = 4;
+const int IN2 = 5;
+const int IN3 = 6;
+const int IN4 = 7;
+
+void setup() {
+  servo1.attach(11);
+  servo2.attach(3);
+  servo1.write(currentAngle);
+  servo2.write(currentAngle);
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+}
+
+void driveline(int targetspeed, int direction) {
+  // Steering
+  if (currentAngle < direction) {
+    currentAngle++;
+    servo1.write(currentAngle);
+    servo2.write(currentAngle);
+    delay(5);
+  } else if (currentAngle > direction) {
+    currentAngle--;
+    servo1.write(currentAngle);
+    servo2.write(currentAngle);
+    delay(5);
+  }
+
+  // Motor Direction
+  if (targetspeed > 0) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  } else if (targetspeed < 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  } else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+
+  // Speed Ramp
+  
+  if (currentspeed < targetspeed) {
+    currentspeed += 1;
+    if (currentspeed > targetspeed) currentspeed = targetspeed;
+  } else if (currentspeed > targetspeed) {
+    currentspeed -= 1;
+    if (currentspeed < targetspeed) currentspeed = targetspeed;
+  }
+
+  analogWrite(ENA, abs(currentspeed));
+  analogWrite(ENB, abs(currentspeed));
+}
+
+int distos(int distance) {
+    int speed;
+    if (distance <= 1) {
+        targetspeed = 0;
+    } else if (distance <= 5) {
+        targetspeed = 100;
+    } else {
+        targetspeed = 255;
+    }
+    return targetspeed;
+}
+
+void loop() {
+  //uwb
+  targetspeed = distos(distance); // convert distance to a speed 
+  driveline(targetspeed ,direction); // speed angle
+  delay(100); // Run every 100ms (adjust as needed)
+
+}
